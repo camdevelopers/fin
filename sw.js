fb8c395280292e5382b2c93de809b7a7e6b0e6fa
camdevelopers <camroom@proton.me>
2026-09-09 20:51:02 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 const CACHE = "trust-expenses-v1";
-const SHELL = ["/", "/index.html", "/manifest.json", "/icon-192.png"];
+const SHELL = [".", "./index.html", "./manifest.json", "./icon-192.png"];   
 
 self.addEventListener("install", e => {
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(SHELL)));
